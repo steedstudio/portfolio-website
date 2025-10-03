6024a36bda06f5b0441d53139f90ee9c37de7fd2
Steed <steedstudio@proton.me>
2025-10-03 16:45:06 -0500

    Removed unused import from layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Mulish } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import Head from "next/head";
 
 const mulish = Mulish({
   subsets: ["latin"]
